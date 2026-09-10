f9a6f51eca1deaf5924a507afa6c236542711485
AlishaAttar <attaralisha12@gmail.com>
2026-09-10 22:16:14 +0530

    basic python logic problems

---
+++ basic_logic.py
@@ -0,0 +1,24 @@
+
+#practice problem 1 checking eligibility for student discount
+#age <25 and Student =true
+#age=int(input("Enter your age:"))
+#student=(input("Are you a student:"))=="True"
+#ans=(age <25 and (student))
+#print("Eligible=",ans)
+
+#practice problem 2 driving eligibility 
+#age >=18
+
+#age1=int(input("Enter your age:"))
+#result = (age1>=18)
+#print("Eligible=" ,result)
+
+#num=int(input("Enter your number:"))
+#ans=(num%100)
+#print("Last two digits =",ans)
+
+second=int(input("Enter number of seconds:"))
+result=second//60
+ans = second%60
+print("Minutes:",result)
+print("seconds",ans)
--- practice 6.py
+++ practice 6.py
@@ -1,5 +1,5 @@
 a=int(input("Enter the vakue of a:"))
-b= int(input("Enter the value of b"))
+b= int(input("Enter the value of b:"))
 a,b=b,a
 print("a=",a)
 print("b=",b)
