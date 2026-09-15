c344c11c4fa93c68b01e4fea77cc1e64ea5ade82
AlishaAttar <attaralisha12@gmail.com>
2026-09-15 21:24:34 +0530

    python program for finding avg of list

---
+++ questions.py
@@ -0,0 +1,25 @@
+# display numbers from -10 to -1
+for i in range (-10,-1):
+     print(i)
+
+
+# calculate the sum of all numbers from 1 to n in a given list
+l=[1,2,3,4,5]
+sum = 0 
+for i in l:
+      sum = sum+i
+print("The sum is ",sum)
+
+
+
+#write a program to calculate avg of a list
+l =[1,2,3,4,5]
+sum =0
+for i in l:
+      sum = sum + i
+      avg = sum/len(l)
+      print("Avg =",avg)
+
+
+# write a calculator program that has a menu system where it asks for a choice from user (+,-,*,/,factorial 
+#it should display the op utput until the user explicitly terminated the program by writing exit 
